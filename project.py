3fae828f170100df6b9d0b197ba47b87f8dc571e
jmille36 <38020377+jmille36@users.noreply.github.com>
2018-04-22 19:18:04 -0400

    Add files via upload

--- project.py
+++ project.py
@@ -107,66 +107,93 @@ class PrefPage(Frame):
         purp_chebut_2 = Checkbutton(self, text= "Internship", variable = purp_che_2)
         purp_chebut_2.grid(row=1, column=4, columnspan=3, padx=5, pady=5)
 
+
+        credit_type = Label(self, text="Credits")
+        credit_type.grid(row=2, column=0, padx=5, pady=5)
+        resident_cr = IntVar()
+        resident_credit = Checkbutton(self, text = "Resident Credit", variable = resident_cr)
+        resident_credit.grid(row=2, column=1, columnspan=3, padx=5, pady=5)
+        transfer_cr = IntVar()
+        transfer_credit = Checkbutton(self, text= "Transfer Credit", variable = transfer_cr)
+        transfer_credit.grid(row=2, column=4, columnspan=3, padx=5, pady=5)
+
+
         reg_lab = Label(self, text="Region")
-        reg_lab.grid(row=2, column=0, padx=5, pady=5)
-        reg_option = ["Africa - Central", "Africa - East", "Africa - North", "Africa - South", "Africa - West",
-                        "America - Central", "America - North", "America - South", "Asia - Central", "Asia - East",
-                        "Asia - South", "Asia - SouthEast","Asia - West", "Caribbean", "Europe - East",
-                        "Europe - North", "Europe - South", "Europe - West", "Middle East","Oceania"]
+        reg_lab.grid(row=3, column=0, padx=5, pady=5)
+        reg_option = ["Europe", "Africa", "Asia", "South America"]
         reg_var = StringVar(self)
         reg_var.set("Select...")
         reg_en = OptionMenu(self, reg_var, *reg_option)
-        reg_en.grid(row=2, column=1, columnspan=5, padx=5, pady=5)
-
+        reg_en.grid(row=3, column=1, columnspan=5, padx=5, pady=5)
+		
+		#add a checkbox - want to learn/speak another language? if yes, display second languages. if no, do not.
         seclang_lab = Label(self, text="Second Languages")
-        seclang_lab.grid(row=3, column=0, padx=5, pady=5)
-        seclang_option = ["None","Arabic", "French", "German", "Hindi", "Italian", "Japanese", "Korean", "Mandarin",
-                        "Portuguese", "Russian", "Spanish", "Vietnamese"]
+        seclang_lab.grid(row=4, column=0, padx=5, pady=5)
+        seclang_option = ["None","French", "German", "Italian", "Korean", "Portuguese", "Spanish"]
         seclang_var = StringVar(self)
         seclang_var.set("Select...")
         seclang_en = OptionMenu(self, seclang_var, *seclang_option)
-        seclang_en.grid(row=3, column=1, columnspan=5, padx=5, pady=5)
+        seclang_en.grid(row=4, column=1, columnspan=5, padx=5, pady=5)
 
+        # cost range should be a drop down menu
+        # promt: select the maximum program cost that you would be willing to pay
+        # options for drop down menu
+        	# $10,000
+        	# $15,000
+        	# $20,000
+        	# $25,000
+        	# $30,000
+        	# No limit
         cost_lab = Label(self, text="Cost range")
-        cost_lab.grid(row=4, column=0, padx=5, pady=5)
+        cost_lab.grid(row=5, column=0, padx=5, pady=5)
         cost_en_min = Entry(self)
-        cost_en_min.grid(row=4, column=1, columnspan=2, padx=5, pady=5)
+        cost_en_min.grid(row=5, column=1, columnspan=2, padx=5, pady=5)
         to = Label(self, text="to")
-        to.grid(row=4, column=3, columnspan = 2, padx=5, pady=5)
+        to.grid(row=5, column=3, columnspan = 2, padx=5, pady=5)
         cost_en_max= Entry(self)
-        cost_en_max.grid(row=4, column=5, columnspan=2, padx=5, pady=5)
+        cost_en_max.grid(row=5, column=5, columnspan=2, padx=5, pady=5)
 
-        acco_lab = Label(self, text="Accomodation")
-        acco_lab.grid(row=5, column=0, padx=5, pady=5)
-        acco_option = ["Apartment", "Campus", "Host"]
+        acco_lab = Label(self, text="Housing Accomodation Preference")
+        acco_lab.grid(row=6, column=0, padx=5, pady=5)
+        acco_option = ["Apartment", "Campus", "Host", "No Preference"]
         acco_var = StringVar(self)
         acco_var.set("Select...")
         acco_en = OptionMenu(self, acco_var, *acco_option)
-        acco_en.grid(row=5, column=1, columnspan=5, padx=5, pady=5)
+        acco_en.grid(row=6, column=1, columnspan=5, padx=5, pady=5)
 
-        trp_lab = Label(self, text="Trips")
-        trp_lab.grid(row=6, column=0, padx=5, pady=5)
+        trp_lab = Label(self, text="Extracurricular Preferences - select all that apply")
+        trp_lab.grid(row=7, column=0, padx=5, pady=5)
         trp_che_1 = IntVar()
         trp_chebut_1 = Checkbutton(self, text="City Exploring", variable = trp_che_1)
-        trp_chebut_1.grid(row=6, column=1, columnspan=2, padx=5, pady=5)
+        trp_chebut_1.grid(row=7, column=1, columnspan=2, padx=5, pady=5)
         trp_che_2 = IntVar()
         trp_chebut_2 = Checkbutton(self, text="Sightseeing", variable = trp_che_2)
-        trp_chebut_2.grid(row=6, column=3, columnspan=2, padx=5, pady=5)
+        trp_chebut_2.grid(row=7, column=3, columnspan=2, padx=5, pady=5)
         trp_che_3 = IntVar()
         trp_chebut_3 = Checkbutton(self, text="Nightlife", variable = trp_che_3)
-        trp_chebut_3.grid(row=6, column=5, columnspan=2, padx=5, pady=5)
+        trp_chebut_3.grid(row=7, column=5, columnspan=2, padx=5, pady=5)
 
         seas_lab = Label(self, text="Program Season")
-        seas_lab.grid(row=7, column=0, padx=5, pady=5)
-        seas_option = ["Spring", "Summer", "Fall", "Winter"]
+        seas_lab.grid(row=8, column=0, padx=5, pady=5)
+        seas_option = ["Fall", "Spring"]
         seas_var = StringVar(self)
         seas_var.set("Select...")
         seas_en = OptionMenu(self, seas_var, *seas_option)
-        seas_en.grid(row=7, column=1, columnspan=5, padx=5, pady=5)
+        seas_en.grid(row=8, column=1, columnspan=5, padx=5, pady=5)
+
+        program_duration = Label(self, text="Program Duration")
+        program_duration.grid(row=9, column=0, padx=5, pady=5)
+        program_duration_option = ["Semester", "Full Year"]
+        program_duration_var = StringVar(self)
+        program_duration_var.set("Select...")
+        program_duration_en = OptionMenu(self, seas_var, *program_duration_option)
+        program_duration_en.grid(row=9, column=1, columnspan=5, padx=5, pady=5)
 
     def assign(self):
         self.values["purpose_1"] = purp_che_1.get()
         self.values["purpose_2"] = purp_che_2.get()
+        self.values["resident_cr"] = resident_cr.get()
+        self.values["transfer_cr"] = transfer_cr.get()
         self.values["region"] = reg_var.get()
         self.values["sec_lang"] = seclang_var.get()
         self.values["cost_min"] = cost_en_min.get()
@@ -176,6 +203,7 @@ class PrefPage(Frame):
         self.values["trip_2"] = trp_che_2.get()
         self.values["trip_3"] = trp_che_3.get()
         self.values["season"] = seas_var.get()
+        self.values["program_duration"] = program_duration_var.get()
         return self.values
 
 class ResultPage(Frame):
@@ -185,6 +213,7 @@ class ResultPage(Frame):
         intro = Label(self,text="Here are our suggestions",font = ("Times New Roman", 12))
         intro.pack(side=TOP)
 
+        # I don't get how the buttons are placed on the page so I messed up the placement ... needs to be fixed
         button_1 = Button(self, text="Back", command=lambda: project.show_frame(PrefPage))
         button_1.pack(side=BOTTOM)
 
